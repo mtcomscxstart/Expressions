27a17e1e6621317f3399d191b9493b40d781d2e6
Valery Tolstov <testautomail@mail.ru>
2015-03-30 01:08:08 +0400

    Add some new tests

--- src/test.py
+++ src/test.py
@@ -6,7 +6,12 @@ class TestParser(unittest.TestCase):
     def test_add(self):
         self.assertEqual(expr.evs('2+2'), 4)
         self.assertEqual(expr.evs('2+2+2'), 6)
-        self.assertEqual(expr.evs('-(2+2)'), 0)
+
+    def test_brackets(self):
+        self.assertEqual(expr.evs('(2+2)'), 4)
+
+    def test_unary(self):
+        self.assertEqual(expr.evs('-(2+2)'), -4)
 
 if __name__ == '__main__':
     unittest.main()
